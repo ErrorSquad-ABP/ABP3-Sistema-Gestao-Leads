a2e3e26127a86f7ee5af8f953f8a53d692ee7787
João Victor Lopes Rosa <joaovlr9@gmail.com>
2026-05-11 22:04:31 -0300

    style(customers): adjust mercado livre icon color

--- front/src/features/customers/components/CustomersManagementScreen.tsx
+++ front/src/features/customers/components/CustomersManagementScreen.tsx
@@ -131,7 +131,7 @@ function SourceIcon({ value }: { readonly value: string }) {
 		case 'MERCADO_LIVRE':
 			return (
 				<Icon
-					className="size-5 text-[#3483fa]"
+					className="size-5 text-[#ffe600]"
 					icon="arcticons:mercado-libre"
 				/>
 			);
